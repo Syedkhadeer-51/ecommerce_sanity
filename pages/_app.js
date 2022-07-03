d1d1bfbde14bc774ca411b802c3dbd00af2e463c
Syedkhadeer-51 <67771257+Syedkhadeer-51@users.noreply.github.com>
2022-07-03 22:31:43 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StatContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <StatContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />  
+      </Layout>
+    </StatContext>
+  ) 
 }
 
 export default MyApp
